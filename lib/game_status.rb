ae35752990757aeb8307a85d5f78aa91945d0b4e
Kim Gonzales <kimgo589@gmail.com>
2018-01-08 19:07:14 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,7 +27,7 @@ def won?(board)
 
       #position_1 == "X" && position_2 == "X" && position_3 == "X" ||
       #position_1 == "O" && position_2 == "O" && position_3 == "O"
-      position_1 == position_2 == position_3
-    
+      position_1 == position_2 || position_1 == position_3 || position_2 == position_3
+
   end
 end
